23e13606f26df92aea8b998deb206e0e08966665
Kaustubh <110345555+KaustubhWahane@users.noreply.github.com>
2022-10-30 22:42:57 +0530

    Add files via upload

---
+++ JavaScript Code with Harry/All operators/Alloperators.js
@@ -0,0 +1,33 @@
+console.log("Welcome to the world of Operators");
+let a = 5;
+let b = 5;
+console.log("a + b: " ,a + b)
+// Note the difference between them because of + it acts as like a String which is concatenation
+console.log("a - b: " +a + b)
+console.log("a * b: " ,a * b)
+console.log("a / b: " ,a / b)
+console.log("a % b: " ,a % b)
+
+// Now the main difference between the increment & decrement operator
+// Increment Operator
+console.log("a++: " ,a++)
+// Notice this when we increment it doesn't print it unless we use another print statement to print it
+console.log("a" , a)
+// Now let' say we want to add it first without printing it again so
+console.log("++a:" , ++a)
+// Decrement Operator
+console.log("--b:" ,b--)
+// Same goes for decrement operator 
+console.log(b)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JavaScript Code with Harry/Variables/VarandLet.js
@@ -0,0 +1,23 @@
+//The main difference when it comes between var and let is the block which means the scope of the variable
+var a = 69
+{
+var a = "Ohio oniichan"
+console.log(a)
+}
+console.log(a)
+// So as you can see i can change the data type at any respective area as possible
+
+// In case of Let 
+let b = 69
+{
+    let b = 'Ohio onichan'
+    console.log(b)
+}
+console.log(b)
+
+
+
+
+
+
+
