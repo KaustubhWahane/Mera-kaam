c3b45ce8a3a56b4eb3354eb0fc131dba058b3d56
Kaustubh <110345555+KaustubhWahane@users.noreply.github.com>
2022-11-17 11:00:19 +0530

    Add files via upload

---
+++ HtmlCssJavaScript/script.js
@@ -0,0 +1,11 @@
+console.time("Simple For Loop");
+for(let i=0;i<5;i++ ){
+    console.log("yokso")
+}
+console.timeEnd("Simple For Loop")
+
+console.time("Simple While Loop");
+for(let i=0;i<5;i++ ){
+    console.log("Aeee lavdyaaaa")
+}
+console.timeEnd("Simple While Loop")
