fdeae0cdeb7ae33a03ea663c339fd91ec9aae309
Kaustubh <110345555+KaustubhWahane@users.noreply.github.com>
2022-11-22 16:10:23 +0530

    Add files via upload

---
+++ JavaScript Code with Harry/HtmlCssJavaScript/script.js
@@ -0,0 +1,56 @@
+/*
+console.time("Simple For Loop");
+for(let i=0;i<5;i++ ){
+    console.log("yokso")
+}
+console.timeEnd("Simple For Loop")
+
+console.time("Simple While Loop");
+for(let i=0;i<5;i++ ){
+    console.log("Aeee lavdyaaaa")
+}
+console.timeEnd("Simple While Loop")
+
+alert("Enter the value of A")
+let a = prompt("Enter anything")
+a = Number.parseInt(a);
+let write = confirm("Kuch aur bakne ka hai kya tujhe?")
+let  b = false;
+if(write != b){
+document.write(a)
+}
+else {
+    document.write("Kuch toh likhne ko do")
+}
+console.log(window)
+
+console.log(document.body)
+document.body.style.background= "purple"
+*/
+
+// let a = prompt("Enter your age")
+// a = Number.parseInt(a)
+// let b;
+// a = alert((a > 18 || a<0) ?"You can drive":"You cannot drive")
+// b = confirm("You still want to say your real age?")
+// b = prompt("Enter your age again")
+// if (b < 0){
+//     console.error("Yeh kya likha hai lavde")
+// }
+let color = prompt("Likh konsa color hona")
+document.body.style.background = color
+
+
+// function ageisNegative(){
+//     return (a +"cannot be negative") 
+// }
+// }
+// while(a > 0){
+// if(a>0&&a<18){
+//    a =prompt("Enter your age real this time")
+//    document.write("So your age is" + a)
+// }
+// else if(a < 0){
+//     console.error("Yeh kya likha hai lavde?")
+// }
+// }
